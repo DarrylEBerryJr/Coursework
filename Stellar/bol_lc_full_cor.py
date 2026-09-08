d5e0d086739355f145726196e751e16ded67e794
Darryl E Berry Jr <deberry@smu.edu>
2026-09-08 00:32:31 -0500

    plots for stellar astro x371 data

---
+++ Stellar/bol_lc_full_cor.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import csv
+
+x = []
+y = []
+z = []
+
+def main():
+
+    with open('/Users/debj/Documents/code/Coursework/Stellar/x371/deberry/sn1ph/bol_lc_full_cor.dat', 'r') as datafile:
+        plotting = csv.reader(datafile, delimiter=' ')
+
+        for rows in plotting:
+            x.append(rows[0])
+            y.append(rows[1])
+            z.append(rows[2])
+
+    chart = input("xy, xz, or yz: ")
+
+    if chart == "xy":
+        print("Graph x=Column 1, y=Column 2")
+        xy()
+    elif chart == "xz":
+        print("Graph x=Column 1, y=Column 3")
+        xz()
+    else:
+        print("Graph x=Column 2, y=Column 3")
+        yz()
+
+def xy():
+    plt.plot(x,y)
+    plt.title('bol_lc_full_cor.dat')
+    plt.xlabel('Column 1')
+    plt.ylabel('Column 2')
+    plt.show()
+
+def xz():
+    plt.plot(x,z)
+    plt.title('bol_lc_full_cor.dat')
+    plt.xlabel('Column 1')
+    plt.ylabel('Column 3')
+    plt.show()
+
+def yz():
+    plt.plot(y,z)
+    plt.title('bol_lc_full_cor.dat')
+    plt.xlabel('Column 2')
+    plt.ylabel('Column 3')
+    plt.show()
+
+main()
