f98af548244170db4d71804aca87d67b7318dc7e
Darryl E Berry Jr <deberry@smu.edu>
2026-09-09 16:40:11 -0500

    flux_to_magn_conv.py made, deleted extraneous flm plot files, added files from Stellar Class

--- Stellar/HET_spectra_plots.py
+++ Stellar/HET_spectra_plots.py
@@ -2,9 +2,11 @@
 import numpy as np
 
 #function made in separate file to call for plotting all flm files
+#plotting_flm_files.py
 from plotting_flm_files import HET_7, HET_10
+#method from https://www.geeksforgeeks.org/python/python-call-function-from-another-file/
 
-#sn1sp flm files are spectra data - before, during, and/or after peak (mostly) from HET at LRS
+#sn1sp flm files are spectra data - before, during, and/or after peak (mostly) from HET using LRS
 #Rest wavelength, scaled flux, error
 
 which = input("Which data to plot? ")
@@ -13,3 +15,9 @@ if which == "7":
     HET_7()
 if which == "10":
     HET_10()
+
+
+
+
+#Code by Darryl E Berry Jr (c) 2026
+#2nd Year PhD Student - SMU
